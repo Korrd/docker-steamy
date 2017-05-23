0175b0f6078c577eabb46f82480bbdc528527fd6
Victor Martin <victor@mural.ly>
2017-05-23 15:24:10 -0300

    Refactor to work with routes instead of env vars

--- index.js
+++ index.js
@@ -1,23 +1,10 @@
+/**
+ * Init and stuff
+ */
+
 const express = require('express');
 const socketio = require('socket.io');
 const spawn = require('child_process').spawn;
-
-const env = process.env;
-const services = env.SERVICES.split(/[\s\t]/);
-const COMMAND = env.COMMAND.split(/[\s\t]/);
-
-const colors = ['red', 'blue', 'green', 'pink'];
-var cc = 0;
-const serviceColors = {};
-const listeners = services.map((servicename) => {
-	serviceColors[servicename] = serviceColors[servicename] || colors[cc++];
-	return {
-		name: servicename,
-		spawned: spawn(COMMAND[0], COMMAND.slice(1).concat([servicename]))
-	}
-});
-
-console.log(serviceColors);
 
 const app = express();
 
@@ -26,41 +13,60 @@ const io = require('socket.io')(server);
 
 const body = require('fs').readFileSync('./index.html', 'utf8');
 
-
-//forces client to connect as websockets. If client tries xhr polling, it won't connect.
-
+var serviceName;
 /**
  * Booting express app
  */
 
 app.use(express.static('./static'));
 
-app.get('/', function (req, res, next) {
-  res.status(200).send(body);
+
+app.get('/logs', function (req, res, next) {
+  res.status(200).send('Error: no serviceId specified. Expecting "/logs/serviceId"');
 });
 
 
-io.set('transports', ['websocket']); 
+
+app.get('/logs/:serviceId', function (req, res, next) {
+	console.log('Endpoint "/logs" hit. Our serviceId is "' + req.params.serviceId + '"');
+
+	var subDomain = req.headers.host.split('.');
+
+	/**
+	 * Validation of our address
+	 */
+
+	if (subDomain[1].toLowerCase().trim() != 'int')
+	{
+		console.log('unexpected input');
+		console.log('subDomain[0]: ' + subDomain[0].toLowerCase().trim());
+		console.log('subDomain[1]: ' + subDomain[1].toLowerCase().trim());
+
+		//We are not on the testing rig, abort.
+	}else{
+		//internal, so our first word on the array must be the PR name.
+		console.log('our service name is ' + req.params.serviceId)
+
+		serviceName = subDomain[0].toString() + '_' + req.params.serviceId;
+		console.log('Our docker service is ' + serviceName);
+	}
+
+  res.status(200).send(body);
+});
+
+console.log('Our service name is ' + serviceName);
+
+const child = spawn('docker',['service', 'logs', '-f', '-t', serviceName]);
+
+// We force our server to use ws only.
+io.set('transports', ['websocket']);
 
 io.on('connection', function(socket) {
 	console.log('websocket connected');
-	listeners.forEach((listener) => {
-		listener.spawned.stdout.on('data', (data) => {
-			socket.emit('stdout', {
-			  servicename: listener.name,
-			  color: serviceColors[listener.name],
-			  data: data.toString()
-			});
-		});
 
-		listener.spawned.stderr.on('data', (data) => {
-			socket.emit('stderr', {
-			  servicename: listener.name,
-			  color: serviceColors[listener.name],
-			  data: data.toString()
-			});
-		});
-	})
+	socket.emit('stdout', child.data.toString());
+	socket.emit('stderr', child.data.toString());
+
 });
 
 
--- static/client.js
+++ static/client.js
@@ -3,10 +3,10 @@ var $main = $('#main');
 var socket = io({ transports: ['websocket'], upgrade: false });
 
 // Whenever the server emits 'data', update the logs
-socket.on('stdout', function (logObject) {
-  $main.append('<code style="color:' + logObject.color + '">[' + logObject.servicename + '] ' + logObject.data + '</code>');
+socket.on('stdout', function (logText) {
+  $main.append(logText);
 });
 
-socket.on('stderr', function (logObject) {
-  $main.append('<code style="color:' + logObject.color + '">[' + logObject.servicename + '] ' + logObject.data + '</code>');
+socket.on('stderr', function (logText) {
+  $main.append(logText);
 });
