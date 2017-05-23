4787a862fe76fdac8578ebc37e885e511a8d1e7e
Victor Martin <victor@mural.ly>
2017-05-23 18:35:04 -0300

    Update client and server

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ const io = require('socket.io')(server);
 
 const body = require('fs').readFileSync('./index.html', 'utf8');
 
-var serviceName;
 /**
  * Booting express app
  */
@@ -22,52 +21,63 @@ app.use(express.static('./static'));
 
 
 app.get('/logs', function (req, res, next) {
-  res.status(200).send('Error: no serviceId specified. Expecting "/logs/serviceId"');
+  const msg = 'Error: no serviceId specified. Expecting "/logs/serviceId"';
+
+  console.log('[SERVER] ' + msg);
+  res.status(200).send(msg);
 });
-
-
 
 app.get('/logs/:serviceId', function (req, res, next) {
-	console.log('Endpoint "/logs" hit. Our serviceId is "' + req.params.serviceId + '"');
-
 	var subDomain = req.headers.host.split('.');
-
-	/**
-	 * Validation of our address
-	 */
-
-	if (subDomain[1].toLowerCase().trim() != 'int')
+	if (subDomain.length <= 1)
 	{
-		console.log('unexpected input');
-		console.log('subDomain[0]: ' + subDomain[0].toLowerCase().trim());
-		console.log('subDomain[1]: ' + subDomain[1].toLowerCase().trim());
-
-		//We are not on the testing rig, abort.
+	    const msg = 'Unexpected input. No subdomain here';
+		console.log('[SERVER] ' + msg);
+		res.status(200).send(msg);
 	}else{
-		//internal, so our first word on the array must be the PR name.
-		console.log('our service name is ' + req.params.serviceId)
-
-		serviceName = subDomain[0].toString() + '_' + req.params.serviceId;
-		console.log('Our docker service is ' + serviceName);
+		console.log('[SERVER] Endpoint "/logs" hit. Our serviceId is "' + req.params.serviceId + '"');
+		console.log('[SERVER] Our PR service name is "' + subDomain[0].toString() + '"');
+		console.log('[SERVER] Our swarm service name is ' + subDomain[0].toString() + '_' + req.params.serviceId + '"');
+		res.status(200).send(body);
 	}
-
-  res.status(200).send(body);
 });
 
-console.log('Our service name is ' + serviceName);
-
-const child = spawn('docker',['service', 'logs', '-f', '-t', serviceName]);
 
 // We force our server to use ws only.
 io.set('transports', ['websocket']);
 
 io.on('connection', function(socket) {
-	console.log('websocket connected');
+	console.log('[SERVER] websocket connected');
 
-	socket.emit('stdout', child.data.toString());
-	socket.emit('stderr', child.data.toString());
+	socket.emit('params');
 
+	socket.on('disconnect', function () {
+
+		//cleanup
+		socket.logprocess.kill();
+
+	});
+
+	socket.on('params', function (value) {
+		console.log('[SERVER] Executing command with swarm service name ' + value);
+		 var child = spawn('docker', ['service', 'logs', '-f', '-t', value]);
+
+		 socket.logprocess = child;
+
+		 child.stdout.on('data', (e) => {
+		 	socket.emit('stdout', e.toString());
+		 });
+
+		 child.stdout.on('end', (a) => {
+		 	console.log('end', a)
+		 });
+
+		 child.stdout.on('error', (e) => console.log('error', e));
+		 child.on('error', (e) => console.log('error', e));
+		 child.on('exit', (e) => console.log('exit', e));
+		 child.on('end', (e) => console.log('end', e));
+
+		});
 });
 
-
-server.listen(3000, () => console.log('server listening'));
+server.listen(3000, () => console.log('[SERVER] listening'));
--- static/client.js
+++ static/client.js
@@ -1,12 +1,34 @@
 var $main = $('#main');
+var $pr_name = $('#pr_name');
 
 var socket = io({ transports: ['websocket'], upgrade: false });
 
 // Whenever the server emits 'data', update the logs
+
+// document.getElementById('pr_name').innerHTML = unValor;
+
+socket.on('params', function (serviceId){
+	console.log('[CLIENT] params triggered');
+	const pr_name = window.location.hostname.split('.')
+	const service = window.location.pathname.split('/')
+
+	console.log('[CLIENT] PR name is ' + pr_name[0]);
+	console.log('[CLIENT] service name is ' + service[service.length - 1].toString());
+
+	socket.emit('params', pr_name[0].toString() + '_' + service[service.length - 1].toString());
+});
+
 socket.on('stdout', function (logText) {
+  console.log('[CLIENT STDOUT] Appending to viewport: ' + logText)
   $main.append(logText);
 });
 
 socket.on('stderr', function (logText) {
+  console.log('[CLIENT STDERR] Appending to viewport: ' + logText)
   $main.append(logText);
 });
+
+
+
+//window.location.hostname
+//window.location.pathname
