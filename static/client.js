115fae848226d31673a4fdc7ae7d1096e19348c7
Victor Martin <victor@mural.ly>
2017-05-24 14:20:08 -0300

    Client refinements

--- static/client.js
+++ static/client.js
@@ -1,16 +1,14 @@
 var $main = $('#main');
-var $pr_name = $('#pr_name');
+var $pr = $('#pr_name');
 
 var socket = io({ transports: ['websocket'], upgrade: false });
-
-// Whenever the server emits 'data', update the logs
-
-// document.getElementById('pr_name').innerHTML = unValor;
 
 socket.on('params', function (serviceId){
 	console.log('[CLIENT] params triggered');
 	const pr_name = window.location.hostname.split('.')
 	const service = window.location.pathname.split('/')
+
+	document.getElementById('pr_name').innerHTML = pr_name[0];
 
 	console.log('[CLIENT] PR name is ' + pr_name[0]);
 	console.log('[CLIENT] service name is ' + service[service.length - 1].toString());
@@ -27,8 +25,3 @@ socket.on('stderr', function (logText) {
   console.log('[CLIENT STDERR] Appending to viewport: ' + logText)
   $main.append(logText);
 });
-
-
-
-//window.location.hostname
-//window.location.pathname
