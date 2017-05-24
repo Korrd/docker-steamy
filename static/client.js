00abb37ca0c97a18259ec69770c3e2b927ff9c37
Victor Martin <victor@mural.ly>
2017-05-24 15:19:05 -0300

    fix style

--- static/client.js
+++ static/client.js
@@ -1,27 +1,64 @@
 var $main = $('#main');
-var $pr = $('#pr_name');
 
 var socket = io({ transports: ['websocket'], upgrade: false });
+
+var autoscroll = true;
+
+/* Put this inside something more accute */
+const header = $('.header'),
+	  main   = $('#main'),
+	  fixup  = 50;
+
+const headerHeight = header.height();
+main.height(window.innerHeight - (headerHeight + fixup));
+/* ##### */
 
 socket.on('params', function (serviceId){
 	console.log('[CLIENT] params triggered');
 	const pr_name = window.location.hostname.split('.')
 	const service = window.location.pathname.split('/')
 
+	document.getElementById('srv_name').innerHTML = service[service.length - 1];
 	document.getElementById('pr_name').innerHTML = pr_name[0];
 
 	console.log('[CLIENT] PR name is ' + pr_name[0]);
-	console.log('[CLIENT] service name is ' + service[service.length - 1].toString());
+	console.log('[CLIENT] service name is ' + service[service.length - 1]);
 
-	socket.emit('params', pr_name[0].toString() + '_' + service[service.length - 1].toString());
+	socket.emit('params', pr_name[0].toString() + '_' + service[service.length - 1]);
 });
 
 socket.on('stdout', function (logText) {
   console.log('[CLIENT STDOUT] Appending to viewport: ' + logText)
+  var objDiv = document.getElementById("main");
+
+  if (objDiv.scrollTop == objDiv.scrollHeight){
+    autoscroll = true;
+  }else{
+    autoscroll = false;
+  }
+
   $main.append(logText);
+
+  if (autoscroll == true){
+	objDiv.scrollTop = objDiv.scrollHeight;
+  }
 });
 
 socket.on('stderr', function (logText) {
   console.log('[CLIENT STDERR] Appending to viewport: ' + logText)
+  var objDiv = document.getElementById("main");
+
+  if (objDiv.scrollTop == objDiv.scrollHeight){
+    autoscroll = true;
+  }else{
+    autoscroll = false;
+  }
+
   $main.append(logText);
+
+  if (autoscroll == true){
+	objDiv.scrollTop = objDiv.scrollHeight;
+  }
+
+
 });
